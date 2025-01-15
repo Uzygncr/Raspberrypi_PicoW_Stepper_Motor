b7314ad84d84bba699b6dcac3f2fa098114a09b0
Uzygncr <genceruzay@gmail.com>
2025-01-15 18:36:53 +0300

    Step Motor Driver

---
+++ build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/uzygncr/pico/pico-projects/Raspberrypi_PicoW_Stepper_Motor/build/_deps/picotool/"
+};
